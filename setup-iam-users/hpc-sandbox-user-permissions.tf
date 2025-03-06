14b458500caba63a79712a660d2d519f8cb03187
Erik Heeren <erik.heeren@openbraininstitute.org>
2025-03-06 11:40:36 +0100

    Permissions for HPC Sandbox account

---
+++ setup-iam-users/hpc-sandbox-user-permissions.tf
@@ -0,0 +1,19 @@
+data "aws_iam_policy_document" "hpc_user_access_policy" {
+  statement {
+    sid    = "HPC User Access Policy"
+    effect = "Allow"
+    principals {
+      type        = "AWS"
+      identifiers = ["130659266700"]
+    }
+    actions = [
+      "ecr-public:DescribeImages",
+      "ecr-public:DescribeRepositories"
+    ]
+  }
+}
+
+resource "aws_ecrpublic_repository_policy" "hpc_user_access" {
+  repository_name = "hpc-resource-provisioner"
+  policy          = data.aws_iam_policy_document.hpc_user_access_policy.json
+}
