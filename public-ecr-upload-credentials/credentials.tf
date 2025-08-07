0665cb4a27bb8515b1da66e268771553ceee1b9f
Daniel Fernández <daferoes@gmail.com>
2025-08-07 13:30:38 +0200

    Split policy into ListObjectsInBucket and AllObjectActions

--- public-ecr-upload-credentials/credentials.tf
+++ public-ecr-upload-credentials/credentials.tf
@@ -93,17 +93,24 @@ resource "aws_iam_policy" "cdn_s3_edit_policy" {
     Version = "2012-10-17",
     Statement = [
       {
+        Sid    = "ListObjectsInBucket"
+        Effect = "Allow"
+        Action = [
+          "s3:ListBucket"
+        ]
+        Resource = "arn:aws:s3:::core-webapp-static-assets-*"
+      },
+      {
+        Sid    = "AllObjectActions"
         Effect = "Allow"
         Action = [
           "s3:PutObject",
+          "s3:PutObjectAcl",
           "s3:GetObject",
-          "s3:DeleteObject",
-          "s3:ListBucket",
-          "s3:ListBucketMultipartUploads",
-          "s3:AbortMultipartUpload"
+          "s3:DeleteObject"
         ]
         Resource = "arn:aws:s3:::core-webapp-static-assets-*/*"
-      },
+      }
     ]
   })
 }
