3ae97e9ffb57b449031a3c2f918030e306d6cd08
Daniel Fernández <daferoes@gmail.com>
2026-02-23 11:26:05 +0100

    Use EOF instead of jsonencode in lifecycle_policy
    https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/ecr_lifecycle_policy.html

--- private-ecr-repo/lifecycle.tf
+++ private-ecr-repo/lifecycle.tf
@@ -2,33 +2,36 @@ resource "aws_ecr_lifecycle_policy" "policy" {
   count      = var.lifecycle_policy_max_image_count != null || var.lifecycle_policy_max_image_age_days != null ? 1 : 0
   repository = aws_ecr_repository.private_repo.name
 
-  policy = jsonencode({
-    rules = concat(
-      var.lifecycle_policy_max_image_count != null ? [{
-        rulePriority = 1
-        description  = "Keep last ${var.lifecycle_policy_max_image_count} images"
-        selection = {
-          tagStatus   = "any"
-          countType   = "imageCountMoreThan"
-          countNumber = var.lifecycle_policy_max_image_count
-        }
-        action = {
-          type = "expire"
-        }
-      }] : [],
-      var.lifecycle_policy_max_image_age_days != null ? [{
-        rulePriority = 2
-        description  = "Delete images older than ${var.lifecycle_policy_max_image_age_days} days"
-        selection = {
-          tagStatus   = "any"
-          countType   = "sinceImagePushed"
-          countUnit   = "days"
-          countNumber = var.lifecycle_policy_max_image_age_days
-        }
-        action = {
-          type = "expire"
-        }
-      }] : []
-    )
-  })
+  policy = <<EOF
+{
+  "rules": [
+    ${var.lifecycle_policy_max_image_count != null ? jsonencode({
+  rulePriority = 1
+  description  = "Keep last ${var.lifecycle_policy_max_image_count} images"
+  selection = {
+    tagStatus   = "any"
+    countType   = "imageCountMoreThan"
+    countNumber = var.lifecycle_policy_max_image_count
+  }
+  action = {
+    type = "expire"
+  }
+  }) : ""}
+    ${var.lifecycle_policy_max_image_count != null && var.lifecycle_policy_max_image_age_days != null ? "," : ""}
+    ${var.lifecycle_policy_max_image_age_days != null ? jsonencode({
+  rulePriority = 2
+  description  = "Delete images older than ${var.lifecycle_policy_max_image_age_days} days"
+  selection = {
+    tagStatus   = "any"
+    countType   = "sinceImagePushed"
+    countUnit   = "days"
+    countNumber = var.lifecycle_policy_max_image_age_days
+  }
+  action = {
+    type = "expire"
+  }
+}) : ""}
+  ]
+}
+EOF
 }
