bf2d97389e27b7f3bc7e14ee5d8ea1bbbed7458e
Erik Heeren <erik.heeren@openbraininstitute.org>
2026-04-08 10:48:24 +0200

    New launch-system executor execuction roles

--- main.tf
+++ main.tf
@@ -337,8 +337,9 @@ module "launch_executor" {
 
   repository_name = "launch-system/default-executor"
   allowed_to_pull_principals = { AWS = [
-    "arn:aws:iam::992382665735:role/launch_system_executor20251120132453317700000009", # staging
-    "arn:aws:iam::671250183987:role/launch_system_executor20260402094333906500000006", # production
+    "arn:aws:iam::992382665735:role/launch_system_executor20260408081519816200000001", # staging default executor
+    "arn:aws:iam::992382665735:role/launch_system_executor20251120132453317700000009", # staging inait executor
+    "arn:aws:iam::671250183987:role/launch_system_executor20260402094333906500000006", # production - to be updated. And add inait executor!
   ] }
   lifecycle_policy_max_image_count    = 10
   lifecycle_policy_max_image_age_days = 30
